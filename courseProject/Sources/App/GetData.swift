43624a7ca7cf82f8f4f7e5fad3106cf0e3e5a832
KabStas <kabstas@rambler.ru>
2020-12-22 14:30:16 +0000

    changes for deploy

--- courseProject/Sources/App/GetData.swift
+++ courseProject/Sources/App/GetData.swift
@@ -3,14 +3,25 @@ import Foundation
 class GetData: GetDataProtocol {
 
     func creatingDictionary() -> [String: [String: String]] {
-        let path = Bundle.module.path(forResource: "dict", ofType: "json") ?? "dict.json"
-        var dictionary: [String: [String: String]]
-        let decoder = JSONDecoder()
-        if let json = FileManager.default.contents(atPath: path) { 
-            dictionary = (try? decoder.decode([String: [String: String]].self, from: json)) ?? [:]
-        } else {
-            dictionary = [:]
-        }
+
+        var dictionary: [String: [String: String]] = ["cat": ["esp": "Gato",
+                                                              "rus": "Кот",
+                                                              "fr": "Le chat"],
+                                                      "dog": ["esp": "Perro",
+                                                              "rus": "Собака",
+                                                              "fr": "La chienne"],
+                                                      "mouse": ["esp": "Ratón",
+                                                              "rus": "Мышь",
+                                                              "fr": "La souris"]]    
+        
+        // let path = Bundle.module.path(forResource: "dict", ofType: "json") ?? "dict.json"
+        // var dictionary: [String: [String: String]]
+        // let decoder = JSONDecoder()
+        // if let json = FileManager.default.contents(atPath: path) { 
+        //     dictionary = (try? decoder.decode([String: [String: String]].self, from: json)) ?? [:]
+        // } else {
+        //     dictionary = [:]
+        // }
         return dictionary
     }
 }
